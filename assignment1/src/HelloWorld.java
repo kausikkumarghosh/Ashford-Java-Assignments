a9ee514d78cff065ebdc5f55f4d3cd7583f41ede
kausikkumarghosh <kausikkumarghosh@gmail.com>
2019-10-24 20:48:25 -0400

    Assignment 2 initial version

---
+++ assignment1/src/HelloWorld.java
@@ -0,0 +1,22 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("HELLO ASHFORD STUDENTS, THIS IS KAUSIK!");
+        /**
+         NAME: Kausik Ghosh
+         COURSE ID: ISM510
+         DATE: October 17, 2019
+         */
+        System.out.println("A data type specifies a set of values and operations");
+        System.out.println("A variable is a program component used to store or represent data");
+        System.out.println("An arithmetic expression combines operands, operators, and parentheses  ");
+        int result = (3 * 7) % (3 - 4 - 6);
+        System.out.println("result is " + result);
+        System.out.println("Flow of control is the order in which a program performs actions ");
+        if (result > 0)
+            System.out.println("Remainder!");
+
+        else
+            System.out.println("No Remainder!");
+    }
+}
